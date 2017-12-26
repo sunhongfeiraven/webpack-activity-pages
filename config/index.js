413b35ccf5db8b6d0d9110800be40d2dee1de1f3
sunhf <sunhf01@jcgroup.com.cn>
2017-12-26 15:59:03 +0800

    feat: init

--- config/index.js
+++ config/index.js
@@ -5,7 +5,7 @@ const path = require('path')
 module.exports = {
   fileName: 'vue',
   // 额外没有main.js的文件打包
-  additionalFiles: ['privacy', 'lib'],
+  additionalFiles: ['privacy', 'lib', 'assets'],
   // 不需要打包的项目 即不会出现在dist当中
   exceptFiles: ['projectB'],
   // 第三方包
